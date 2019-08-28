5a65076441e053f9768e3af165425926c4a89b20
Arne Pedersen <arne.lamo@gmail.com>
2019-08-28 12:45:05 +0200

    React HMR Bolierplate

---
+++ src/app.js
@@ -0,0 +1,22 @@
+
+import {hot} from "react-hot-loader";
+import React, { Component} from "react";
+import "./App.css";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1> Arne's React Boilerplate </h1>
+        <b>Toolchain:</b>
+        <ul>
+          <li>Bundler: Webpack</li>
+          <li>Compiler: Babel</li>
+          <li>RHMR: React-hot-loader</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
