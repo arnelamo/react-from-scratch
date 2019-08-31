058b69f9fdef4ded0ea36797f28fe778b5dfbb39
Arne Pedersen <arne.lamo@gmail.com>
2019-08-31 17:59:23 +0200

    Index.html update

--- src/app.js
+++ src/app.js
@@ -4,20 +4,20 @@ import React, { Component} from "react";
 import "./App.css";
 
 class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Arne's React Boilerplate </h1>
-        <b>Toolchain:</b>
-        <ul>
-          <li>Bundler: Webpack</li>
-          <li>Compiler: Babel</li>
-          <li>HMR: React-hot-loader</li>
-          <li>Linter: ESlint</li>
-        </ul>
-      </div>
-    );
-  }
+    render(){
+        return(
+            <div className="App">
+                <h1> Arne's React Boilerplate </h1>
+                <b>Toolchain:</b>
+                <ul>
+                    <li>Bundler: Webpack</li>
+                    <li>Compiler: Babel</li>
+                    <li>HMR: React-hot-loader</li>
+                    <li>Linter: ESlint</li>
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default hot(module)(App);
