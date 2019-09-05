9b8eb897ce51dd41cdb54d0e182d93f5dd78f531
Arne Pedersen <arne.lamo@gmail.com>
2019-09-05 19:39:22 +0200

    logo

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@
 import {hot} from "react-hot-loader";
 import React, { Component} from "react";
 import "./App.css";
+import Logo from "./react-logo.png"
 
 class App extends Component{
     render(){
         return(
             <div className="App">
+                <img src={Logo} className="pulse" alt=""/>
                 <h1> Arne's React Boilerplate </h1>
-                <b>Toolchain:</b>
+                <p>Toolchain:</p>
                 <ul>
                     <li>Bundler: Webpack</li>
                     <li>Compiler: Babel</li>
